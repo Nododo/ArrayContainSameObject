846508e3853996926fa7a71185bfad95d74c3ab6
Nododo <nododo1027@gmail.com>
2015-09-15 11:48:09 +0800

    Initial Commit

---
+++ ArrayContainSameObject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArrayContainSameObject
+//
+//  Created by 杜 维欣 on 15/9/15.
+//  Copyright (c) 2015年 nododo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ArrayContainSameObjectTests/ArrayContainSameObjectTests.m
@@ -0,0 +1,40 @@
+//
+//  ArrayContainSameObjectTests.m
+//  ArrayContainSameObjectTests
+//
+//  Created by 杜 维欣 on 15/9/15.
+//  Copyright (c) 2015年 nododo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ArrayContainSameObjectTests : XCTestCase
+
+@end
+
+@implementation ArrayContainSameObjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
