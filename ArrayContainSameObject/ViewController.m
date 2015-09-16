d900c1f20363683bebad85e0fa3759329d0f3573
Nododo <nododo1027@gmail.com>
2015-09-16 11:57:45 +0800

    判断两个数组内的相同对象 最笨办法
    init commit

--- ArrayContainSameObject/ViewController.m
+++ ArrayContainSameObject/ViewController.m
@@ -9,19 +9,58 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic,strong)NSArray *firstArray;
+@property (nonatomic,strong)NSArray *secondArray;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.firstArray = @[@"iOS",@"Java",@"Ruby"];
+    self.secondArray = @[@".Net",@"Php",@"iOS"];
+    NSLog(@"%@",[self justSameObjectWithFirstArray:self.firstArray secondArray:self.secondArray]);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSArray *)justSameObjectWithFirstArray:(NSArray *)firstArray secondArray:(NSArray *)secondArray
+{
+    NSMutableArray *tempArray = [NSMutableArray array];
+    for (NSString *firstStr in firstArray) {
+        BOOL same = NO;
+        for (NSString *secondStr in secondArray) {
+            if ([firstStr isEqualToString:secondStr]) {
+                same = YES;
+            }
+        }
+        if (same == YES) {
+            
+            [tempArray addObject:firstStr];
+        }else
+        {
+        }
+    }
+    
+    for (NSString *firstStr in secondArray) {
+        BOOL same = NO;
+        for (NSString *secondStr in firstArray) {
+            if ([firstStr isEqualToString:secondStr]) {
+                same = YES;
+            }
+        }
+        if (same == YES) {
+            
+            for (NSString *tempStr in tempArray) {
+                if (![tempStr isEqualToString:firstStr]) {
+                    
+                    [tempArray addObject:firstStr];
+                }
+            }
+        }else
+        {
+            
+        }
+    }
+    return tempArray;
 }
 
 @end
